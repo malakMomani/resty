9e91970d8f716cf8f80c13f0adc7bc17593bfdbe
Malak Al-Momani <malak.momani.96@gmail.com>
2021-06-16 20:50:58 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -5,31 +5,75 @@ import Footer from './Footer';
 import Form from './Form';
 import Result from './Result';
 import History from './History.js'
+import If from './If';
+import Loader from './Loader';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      results: [],
+      results: {},
       count: 0,
-      history:[]
+      counter: 1,
+      url: '',
+      method: 'get',
+      body: '',
+      flag: false
     };
   }
 
-  handleForm = (results, count ,operation) => {
-    this.state.history.push(operation);
-    this.setState({ results, count});
+  handleUrl = (url) => {
+    this.setState({ url });
+  }
+
+  handleMethod = (method) => {
+    console.log(method, '-----------')
+    this.setState({ method });
+
+  }
+  handleBody = (body) => {
+    this.setState({ body });
+
+  }
+  handleForm = (results, count, counter, url, method) => {
+    this.setState({ results, count, counter, url, method });
+  }
+
+  historyHandler = (results, url, method, count) => {
+    this.setState({ results ,url, method , count })
+  }
+
+  toggleLoader = () => {
+    this.setState({ flag: !this.state.flag });
+
   }
 
   render() {
     return (
-        <div className="App">
-          <Header />
-          <Form handler={this.handleForm}/>
-          <Result results={this.state.results} />
-          <History history={this.state.history} handler={this.handleHistory}/>
-          <Footer />
-        </div>
+      <div className="App">
+        <Header />
+        <Form handler={this.handleForm}
+          counter={this.state.counter}
+          url={this.state.url}
+          method={this.state.method}
+          handleUrl={this.handleUrl}
+          handleMethod={this.handleMethod}
+          handleBody={this.handleBody}
+          toggleLoader={this.toggleLoader}
+        />
+        <If condition={this.state.flag}>
+          <Loader />
+        </If>
+
+        <History historyHandler={this.historyHandler}  
+                 toggleLoader={this.toggleLoader}
+                 />
+
+        <If condition={this.state.results}>
+            <Result results={this.state.results} />
+        </If>
+        <Footer />
+      </div>
     )
   }
 }
--- src/History.js
+++ src/History.js
@@ -1,28 +1,65 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Form from './Form';
 import './scss/History.scss';
 
-class History extends React.Component {
+class History extends Form{
 
-  handleClick() {
+  handleClick = async (url, method) => {
+    // set the state of the parent with these method and url
+    let raw;
+    this.props.toggleLoader();
+      if (method === 'get') {
+        raw = await fetch(url, {
+          method: method,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          mode: 'cors',
+        });
+      }
+      else {
+        raw = await fetch(url, {
+          method: method,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          mode: 'cors',
+          body: JSON.stringify(''),
+
+        });
+
+      }
+      let data = await raw.json();
+      const count = data.count;
     
+      this.props.historyHandler({data, url, method, count});
+      this.props.toggleLoader(true);
   }
 
-  render(){
-    console.log('history------>' , this.props.history);
+  // we will get from localstorage data --> array of objects
+  // we will loop throw the objects using map
+  renderHistory() {
+    let values = [],
+      keys = Object.keys(localStorage),
+      i = keys.length;
+
+    while (i--) {
+      values.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+    console.log('-------local storage', values);
+    return values.map((item , index) => {
+      console.log('item----------->',item)
+      return (
+        <p key = {index} onClick={() => this.handleClick(item.url, item.method)}>{item.method} -- {item.url}</p>
+      )
+    });
+  }
+  render() {
     return (
       <section id="history">
-        <ul id="history-list">
-          {
-            this.props.history.map((item, idx) =>{
-              return (
-              <li key={idx} onClick={this.handleClick}>
-                <span>{item.method}:</span> <span>{item.url}</span>
-              </li>
-              )
-            })
-          }
-        </ul>
+        <h2>History</h2>
+        {this.renderHistory()}
       </section>
     );
   }
